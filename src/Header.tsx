fc171f8d224a82f2ad2d24b58f7c8044e467646c
jkvyff <kristianvyff@gmail.com>
2019-12-23 12:55:00 -0500

    remove body info to test layout

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,15 +9,15 @@ export const Header: React.FC<Props> = () => {
     const { data, loading } = useMeQuery();
     const [logout, { client }] = useLogoutMutation();
 
-    let body: any = null;
+    // let body: any = null;
 
-    if (loading) {
-        body = null
-    } else if (data && data.me) {
-        body = <div>You are logged in as: {data.me.email}</div>
-    } else {
-        body = <div>You are not logged in</div>
-    }
+    // if (loading) {
+    //     body = null
+    // } else if (data && data.me) {
+    //     body = <div>You are logged in as: {data.me.email}</div>
+    // } else {
+    //     body = <div>You are not logged in</div>
+    // }
 
     return (
         <header className="nav">
@@ -49,7 +49,7 @@ export const Header: React.FC<Props> = () => {
                     }}>logout</button>
                 ) : null}
             </div>
-            {body}
+            {/* {body} */}
         </header>
     );
 }
