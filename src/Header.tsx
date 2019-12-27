5ffd8db9b09b8b96f46e7866e9cafc2aa6108605
jkvyff <kristianvyff@gmail.com>
2019-12-26 18:02:13 -0800

    tests of dropdown nav

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import { useMeQuery, useLogoutMutation } from './generated/graphql';
 import { setAccessToken } from './accessToken';
@@ -8,6 +8,7 @@ interface Props {}
 export const Header: React.FC<Props> = () => {
     const { data, loading } = useMeQuery();
     const [logout, { client }] = useLogoutMutation();
+    const [isOpen, setIsOpen] = useState(false);
 
     // let body: any = null;
 
@@ -18,6 +19,9 @@ export const Header: React.FC<Props> = () => {
     // } else {
     //     body = <div>You are not logged in</div>
     // }
+    const handleToggle = (ev: any) => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <header className="nav">
@@ -31,10 +35,22 @@ export const Header: React.FC<Props> = () => {
                 <form>
                     <input className="search"
                     type="text"
-                    placeholder="Can't searching yet..."/>
+                    placeholder="Can't search yet..."/>
                 </form>
             </div>
-            <div className="nav-link right last">
+            <div className="relative nav-link py-3 px-4 float-right last">
+                <button onClick={handleToggle} className="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
+                    <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar"/>
+                </button>
+                {isOpen && <>
+                <button onClick={handleToggle} className="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
+                <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+                <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account</a>
+                <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</a>
+                </div>
+                </>}
+            </div>
+            <div className="nav-link right">
                 <NavLink to="/helloLock" activeClassName="active">Hello {data && data.me && data.me.username}</NavLink>
             </div>
             <div className="nav-link right">
