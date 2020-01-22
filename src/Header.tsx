12fa632fdb2aa44b02a5979053761c54893119f5
jkvyff <kristianvyff@gmail.com>
2020-01-21 22:33:44 -0800

    convert nav css to tailwind

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useMeQuery, useLogoutMutation } from './generated/graphql';
 import { setAccessToken } from './accessToken';
 
@@ -22,39 +22,47 @@ export const Header: React.FC<Props> = () => {
     const handleToggle = (ev: any) => {
         setIsOpen(!isOpen)
     }
-
+    // .nav-link a {
+    //     display: inline;
+    //     float: left;
+    //     display: block;
+    //     color: white;
+    //     text-align: center;
+    //     padding: 14px 16px;
+    //     text-decoration: none;
+    //   }
     return (
-        <header className="nav">
-            <div className="nav-link first">
+        <header className="flex items-center flex-wrap bg-gray-800">
+            <div className="text-gray-200 flex-initial ml-32 px-2">
                 <NavLink exact to="/" activeClassName="active">Home</NavLink>
             </div>
-            <div className="nav-link">
+            <div className="text-gray-200 flex-initial px-2">
                 <NavLink exact to="/discover" activeClassName="active">Discover</NavLink>
             </div>
-            <div className="nav-link">
+            <div className="text-gray-200 flex-1 mr-48">
                 <form>
-                    <input className="search"
+                    <input className="search w-full rounded flex-1 py-1 px-2"
                     type="text"
                     placeholder="Can't search yet..."/>
                 </form>
             </div>
-            <div className="relative nav-link py-3 px-4 float-right last">
+            <div className="text-gray-200 flex-initial px-2">
+                <NavLink to="/helloLock" activeClassName="active">Hello {data && data.me && data.me.username}</NavLink>
+            </div>
+            <div className="text-gray-200 flex-initial px-2">
+                <NavLink to="/login" activeClassName="active">Login</NavLink>
+            </div>
+            <div className="relative nav-link px-2 py-3 mr-32">
                 <button onClick={handleToggle} className="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
                     <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar"/>
                 </button>
                 {isOpen && <>
                 <button onClick={handleToggle} className="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"></button>
                 <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
-                <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account</a>
-                <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</a>
+                <Link to="/discover" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account</Link>
+                <Link to="/" className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</Link>
                 </div>
                 </>}
-            </div>
-            <div className="nav-link right">
-                <NavLink to="/helloLock" activeClassName="active">Hello {data && data.me && data.me.username}</NavLink>
-            </div>
-            <div className="nav-link right">
-                <NavLink to="/login" activeClassName="active">Login</NavLink>
             </div>
             <div>
                 {!loading && data && data.me ? (
